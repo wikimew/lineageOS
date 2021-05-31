0ea81a104ddbd498dd2e4f8f29277275a18c463b
wikimew <wikili2003@yandex.com>
2021-05-31 09:23:15 +0000

    remove sudo

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo apt-get update
-sudo apt-get install -y libwxgtk3.0-gtk3-dev
-sudo apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
+apt-get update
+apt-get install -y libwxgtk3.0-gtk3-dev
+apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
 mkdir -p android/lineage
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
